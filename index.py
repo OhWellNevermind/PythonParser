bd0cbf24fb7f606d85b7059c7858203fc5a50ab9
OhWellNevermind <aleksandrov.oleksii.wgm@gmail.com>
2024-02-27 21:36:52 +0200

    add write to csv file,browser opens once

--- index.py
+++ index.py
@@ -11,25 +11,28 @@ import requests
 
 downloads_path = str(Path.home() / "Downloads")
 path_to_download = os.path.join(downloads_path, "apks")
+csv_file = "output.csv"
 
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory": path_to_download}
+options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(options)
 
-def check_if_exist(driver, by, selector):
+def check_if_exist(by, selector):
    try:
       driver.find_element(by, selector)
       return True
    except:
       return False
 
-
 def get_bundle_ids():
    ids = []
-   with open("output.csv", newline="", encoding="utf-8") as csvfile:
+   with open(csv_file, newline="", encoding="utf-8") as csvfile:
       reader = csv.DictReader(csvfile)
       for row in reader:
          ids.append(row["bundle id"].split("/")[3])
 
    return ids
-
 
 def get_apk_links_list(list):
    li_arr = []
@@ -45,13 +48,11 @@ def get_apk_links_list(list):
    finally:
       return li_arr
 
-
 def file_exists(path):
    def _predicate(_):
       return Path(path).is_file()
 
    return _predicate
-
 
 def wget(url, output_file):
    try:
@@ -65,21 +66,9 @@ def wget(url, output_file):
    except Exception as e:
         print(f"Помилка під час завантаження: {str(e)}")
 
-
 def download_files(bundle_id):
-   options = webdriver.ChromeOptions()
-   prefs = {"download.default_directory": path_to_download}
-   options.add_experimental_option("prefs", prefs)
-   driver = webdriver.Chrome(options)
-
-   # time.sleep(5)
-   # window_before = driver.window_handles[0]
-   # driver.switch_to.window(window_before)
-
    try:
       driver.get(f"https://apkcombo.com/downloader/#package={bundle_id}")
-      # generate_link_elem = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="apksumbit"]')))
-      # generate_link_elem.click()
       list_architectures = WebDriverWait(driver, 20).until(
          EC.element_to_be_clickable((By.XPATH, '//*[@id="apkcombo-tab"]/div'))
       )
@@ -98,22 +87,23 @@ def download_files(bundle_id):
       filename = os.path.basename(file.path)
       filename = unquote(filename)
       wget(href, f'{path_to_download}/{link[1]}_{filename.replace(':', '_')}')
-    #  if(idx > 0):
-    # #   print(f'{path_to_download}/{os.path.splitext(filename)[0]} ({idx}).apk')
-    # #   WebDriverWait(driver, 60).until(file_exists(f'{path_to_download}/{os.path.splitext(filename)[0].replace(':', '_')} ({idx}).apk'))
-    #  else:
-    #   wget(href, f'{path_to_download}/{filename.replace(':', '_')}')
-    # #   print(f'{path_to_download}/{filename}')
-    # #   WebDriverWait(driver, 60).until(file_exists(f'{path_to_download}/{filename.replace(':', '_')}'))
+      with open('downloaded.csv', 'a',newline='') as file:
+         writer = csv.writer(file)
+         writer.writerow([bundle_id, f'{link[1]}_{filename.replace(':', '_')}'])
 
 
-isExist = os.path.exists(path_to_download)
+def main():
+   is_folder_exist = os.path.exists(path_to_download)
+   with open('downloaded.csv', 'w', newline='') as file:
+      writer = csv.writer(file)
+      writer.writerow(['bundle_id', 'filename'])
 
-if not isExist:
-   os.makedirs(path_to_download)
-bundle_ids = get_bundle_ids()
+   if not is_folder_exist:
+      os.makedirs(path_to_download)
 
-for id in bundle_ids:
-   download_files(id)
+   bundle_ids = get_bundle_ids()
 
-# https://download.apkcombo.com/ua.ukrposhta.android.app/%D0%A3%D0%BA%D1%80%D0%BF%D0%BE%D1%88%D1%82%D0%B0_0.15.70_apkcombo.com.apk?ecp=dWEudWtycG9zaHRhLmFuZHJvaWQuYXBwLzAuMTUuNzAvMTYxLjI0ODk1YTNiYzUxZDVlYmI2NDdiYTllZjIzYTgxYzQyYmM2NDg5ZGYuYXBr&iat=1708971365&sig=a754292ca324adea6842a78211673c76&size=17032137&from=cf&version=latest&lang=en&fp=e782ac5fafc9086043cd0a079e0dcb8f&ip=45.12.24.124
+   for id in bundle_ids:
+      download_files(id)
+
+main()
